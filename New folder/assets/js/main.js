61c9b1abec9fdcc5daa2133475b1b0c78540391e
arifislamarif344 <arifislamarif344@gmail.com>
2020-09-06 12:45:38 +0600

    Added a new file

---
+++ New folder/assets/js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  // Active
+  $(".sideber__content__li").click(function () {
+    $(".sideber__content__li").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  $("#episode__1").click(function () {
+    $(".episode__1").toggleClass("block");
+    var icon = document.querySelector("#episode__1 #icon");
+    icon.classList.toggle("fa-minus");
+  });
+
+  $("#episode__2").click(function () {
+    $(".episode__2").toggleClass("block");
+    var icon = document.querySelector("#episode__2 #icon");
+    icon.classList.toggle("fa-minus");
+  });
+
+  $("#episode__3").click(function () {
+    $(".episode__3").toggleClass("block");
+    var icon = document.querySelector("#episode__3 #icon");
+    icon.classList.toggle("fa-minus");
+  });
+});
+
+// function myFunction(x) {
+//   x.classList.toggle("fa-thumbs-down");
+// }
+
+// var icon = document.querySelector("#icon");
+// icon.addEventListener("click", function () {
+//   icon.classList.toggle("fa-thumbs-down");
+// });
